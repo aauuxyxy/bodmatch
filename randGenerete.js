130acfc10369bdc5bcf644d24887267855a38c7c
uuaaxyxy <uuaaxyxy968@gmail.com>
2023-07-07 02:25:31 +0900

    🏗️ Google認証のみにし、PrismaをMySQLに変更

---
+++ randGenerete.js
@@ -0,0 +1,16 @@
+function generateRandomAlphaNumeric(length) {
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+
+  return result;
+}
+
+// 使用例
+const randomString = generateRandomAlphaNumeric(100);
+console.log(randomString);
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,23 @@
+import { NextApiHandler } from 'next';
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import prisma from '../../../../lib/prisma';
+
+type ClientType = {
+  clientId: string;
+  clientSecret: string;
+};
+
+const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    } as ClientType),
+  ],
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXT_AUTH_SECRET,
+};
+
+export default NextAuth(authOptions);
